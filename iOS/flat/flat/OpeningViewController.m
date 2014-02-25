ed58b3ce80d2063fea82cd52826fdc2ebf143ce0
kylearchie <karchie@stanford.edu>
2014-02-25 15:55:44 -0800

    into page added

--- iOS/flat/flat/OpeningViewController.m
+++ iOS/flat/flat/OpeningViewController.m
@@ -12,13 +12,13 @@
 #import "AuthenticationHelper.h"
 #import "OpeningButton.h"
 #import "GroupTableViewController.h"
+#import "PageContentViewController.h"
 
 @interface OpeningViewController ()
 @property NSArray *readPermissions;
 @end
 
 @implementation OpeningViewController
-
 
 - (void)viewWillAppear:(BOOL)animated
 {
@@ -43,11 +43,87 @@
     [super viewDidLoad];
     [self setNeedsStatusBarAppearanceUpdate];
     
-    //self.screenName = @"Opening View Screen";
+    //start
+    [super viewDidLoad];
+    // Create the data model
+    _pageTitles = @[@"Welcome to your Flat", @"A place to interact with your Flatmates", @"You can see if they are home...", @"and always knows who is busy."];
+    _pageImages = @[@"page1.png", @"page2.png", @"page3.png", @"page4.png"];
+    
+    // Create page view controller
+    self.pageViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"PageViewController"];
+    self.pageViewController.dataSource = self;
+    
+    PageContentViewController *startingViewController = [self viewControllerAtIndex:0];
+    NSArray *viewControllers = @[startingViewController];
+    [self.pageViewController setViewControllers:viewControllers direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:nil];
+    
+    // Change the size of page view controller
+    self.pageViewController.view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height - 30);
+    
+    [self addChildViewController:_pageViewController];
+    [self.view addSubview:_pageViewController.view];
+    [self.pageViewController didMoveToParentViewController:self];
+    //end
+    
+    
     [self.view addSubview:[[OpeningButton alloc] initWithType:@"facebookSignIn"
-                                                             parent:self
-                                                             action:@selector(openFacebookSession)]];
+                                                       parent:self
+                                                       action:@selector(openFacebookSession)]];
     [self setBackground];
+}
+
+- (PageContentViewController *)viewControllerAtIndex:(NSUInteger)index
+{
+    if (([self.pageTitles count] == 0) || (index >= [self.pageTitles count])) {
+        return nil;
+    }
+    
+    // Create a new view controller and pass suitable data.
+    PageContentViewController *pageContentViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"PageContentViewController"];
+    pageContentViewController.imageFile = self.pageImages[index];
+    pageContentViewController.titleText = self.pageTitles[index];
+    pageContentViewController.pageIndex = index;
+    
+    return pageContentViewController;
+}
+
+#pragma mark - Page View Controller Data Source
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController
+{
+    NSUInteger index = ((PageContentViewController*) viewController).pageIndex;
+    
+    if ((index == 0) || (index == NSNotFound)) {
+        return nil;
+    }
+    
+    index--;
+    return [self viewControllerAtIndex:index];
+}
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController
+{
+    NSUInteger index = ((PageContentViewController*) viewController).pageIndex;
+    
+    if (index == NSNotFound) {
+        return nil;
+    }
+    
+    index++;
+    if (index == [self.pageTitles count]) {
+        return nil;
+    }
+    return [self viewControllerAtIndex:index];
+}
+
+- (NSInteger)presentationCountForPageViewController:(UIPageViewController *)pageViewController
+{
+    return [self.pageTitles count];
+}
+
+- (NSInteger)presentationIndexForPageViewController:(UIPageViewController *)pageViewController
+{
+    return 0;
 }
 
 - (UIStatusBarStyle) preferredStatusBarStyle
@@ -65,7 +141,7 @@
 -(void)setBackground {
     UIImageView *background = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height-60)];
     //[background setImage:[UIImage imageNamed:@"opening_background.png"]];
-    [background setBackgroundColor:[UIColor greenColor]];
+    [background setBackgroundColor:[UIColor whiteColor]];
     background.contentMode = UIViewContentModeScaleAspectFill;
     [self.view insertSubview:background atIndex:0];
     self.view.backgroundColor = [UIColor clearColor];
